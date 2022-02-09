00b9c6eb11e498e4c23ecc943e899cc3b1ac46de
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2022-02-09 14:18:07 +0100

    Made user model and accociated admin panel

--- backend/ticking/admin.py
+++ backend/ticking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'username')
+
+# Register your models here.
+
+admin.site.register(User, UserAdmin)
