50cc59d8f9e806130104968c5a997ef27145a26d
Halvor Johan Steine <halvor-steine@hotmail.com>
2022-02-15 15:38:46 +0100

    basic authentication, fixes 403 when frontend POST

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,3 +128,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Denne i backend settings.py løser 403 ? 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
