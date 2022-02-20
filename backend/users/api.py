1b410bab749129232987fa5bb3cf0e6124f07ede
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2022-02-20 13:02:21 +0100

    Django user-model with login and registeration works with api

---
+++ backend/users/api.py
@@ -0,0 +1,50 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterUserSerializer, LoginUserSerializer
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterUserSerializer
+
+    # Can take more arguments
+    def post(self, request, *args, **kwargs):
+        # Data som kommer inne her kommer i serializer
+        serializers = self.get_serializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        user = serializers.save()
+
+        # Serializert bruker
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            #Gjør at man kan logge inn umiddelbart ved registrering. Vet hvem man er utifra token. Kommer i header.
+            "token": AuthToken.objects.create(user)[1]
+        })
+# Login API
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    # Can take more arguments
+    def post(self, request, *args, **kwargs):
+        # Data som kommer inne her kommer i serializer
+        serializers = self.get_serializer(data=request.data)
+        serializers.is_valid(raise_exception=False)
+        user = serializers.validated_data
+        _, token = AuthToken.objects.create(user)
+        # Serializert bruker
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            #Gjør at man kan logge inn umiddelbart ved registrering. Vet hvem man er utifra token. Kommer i header.
+            "token": token
+        })
+# Get User API
+
+class UserAPI(generics.RetrieveAPIView):
+    # Checks if token matches a login token
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
---
+++ backend/users/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email' , 'password')
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+    # TODO: Make class password write only 
+
+    def create(self, validate_data):
+        user = User.objects.create_user(
+            validate_data['username'],
+            validate_data['email'],
+            validate_data['password'],           
+            )
+        
+        return user
+
+# Validere bare en bruker så 
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
+    
