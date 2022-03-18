96d38d43c677b7652f69cfdc2ddfd3c7378c0638
Halvor Johan Steine <halvor-steine@hotmail.com>
2022-03-18 18:22:24 +0100

    Gjør solgte/kjøpte/slettede annonser utilgjengelige
    -Kan markere annonse som solgt dersom du eier annonsen
    -Edit, Delete og contact er ikke mulig på utilgjengelige annonser

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'date', 'location', 'description', 'category', 'user', 'saleOrBuy', 'contactInfo' )
+    list_display = ('title', 'price', 'date', 'location', 'description', 'hidden', 'category', 'user', 'saleOrBuy', 'contactInfo')
 
 # Register your models here.
 
--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -39,6 +39,7 @@ export class Posts extends Component {
         category: "",
         saleOrBuy: "",
         description: "",
+        hidden: true,
         user: "",
         contactInfo: "",
       },
@@ -63,7 +64,9 @@ export class Posts extends Component {
 
   handleSubmitPost = (post) => {
     /* Close window on save */
-    this.toggleCreatePostWindow();
+    this.setState({ modalCreatePost: false });
+    /* this.toggleCreatePostWindow(); */
+
     //IF post exists --> update existing post (PUT-request)
     if (post.id) {
       axios
@@ -117,6 +120,29 @@ export class Posts extends Component {
     });
   };
 
+  /* Setter hidden til true for å gjømme posten, også gjøres en PUT-request for å oppdatere databasen. */
+  hidePost = (existingPost) => {
+    const post = {
+      id: existingPost.id,
+      title: existingPost.title,
+      price: existingPost.price,
+      date: existingPost.date,
+      location: existingPost.location,
+      category: existingPost.category,
+      saleOrBuy: existingPost.saleOrBuy,
+      description: existingPost.description,
+      hidden: true,
+      user: this.props.auth.user.id,
+      contactInfo: this.props.auth.user.email,
+    };
+    this.setState({
+      activePost: post,
+    });
+    /* TODO: dette er ein workaround, activePost: post funke ikkje før andre knappetrykk. vil egentlig submitte this.state.activePost */
+    this.handleSubmitPost(post);
+    /* this.handleSubmitPost(this.state.activePost); */
+  };
+
   /* OBS: Denne kan kun brukes der isAuthenticated == true, aka vi vet at bruker er innlogget */
   canEditPost = (postOwnerId) => {
     /*  alert(postId);
@@ -150,7 +176,14 @@ export class Posts extends Component {
           <Card>
             <CardBody
               /* inverse */
-              style={{ backgroundColor: "#D6DBDF ", borderColor: "#333" }}
+              /* Change card background colour based on if post is hidden or not */
+              style={
+                post.hidden
+                  ? { backgroundColor: "#fc4103", borderColor: "#333" }
+                  : { backgroundColor: "#D6DBDF", borderColor: "#333" }
+              }
+
+              /* style={{ backgroundColor: "#D6DBDF ", borderColor: "#333" }} */
             >
               <CardTitle>
                 {/* Flexbox magi for å få knapp og overskrift på vertikal og horisontal linje + med plass mellom. https://css-tricks.com/snippets/css/a-guide-to-flexbox/  */}
@@ -163,38 +196,69 @@ export class Posts extends Component {
                 >
                   {/* TODO: CardTitle skal egentlig automatisk bli stor, h3 skal ikkje vere nødvendig... */}
                   <h3>{post.title}</h3>
-
-                  {isAuthenticated ? (
+                  {/* TODO: kan ha logikk for å vise enten SOLD eller BOUGHT avhengig av post.saleOrBuy */}
+                  {/* IF hidden --> already sold/bought ELSE see comment below */}
+                  {post.hidden ? (
                     <div>
-                      {/* IF user isAuthenticated and postOwnerId == this.props.auth.user.id ---> show edit button ELSE --> null  */}
+                      <label>SOLD/BOUGHT</label>
+                    </div>
+                  ) : isAuthenticated ? (
+                    <div>
+                      {/* IF user isAuthenticated and postOwnerId == this.props.auth.user.id ---> show edit and delete buttons ELSE --> null  */}
                       {this.canEditPost(post.user) ? (
-                        <button
-                          className="btn"
-                          title="Click here to edit your post" /* denne vises når du svever over knappen */
-                          onClick={() => {
-                            this.toggleCreatePostWindow();
-                            this.editPost(post);
-                            this.setModalTitle("Edit post");
-                          }}
+                        <div>
+                          {/* Delete button */}
+                          <button
+                            className="btn"
+                            /* denne vises når du svever over knappen */
+                            title="Click here to delete your post"
+                            onClick={() => {
+                              /* Submit posten, blir ein PUT-request der hidden=true  */
+                              this.hidePost(post);
+                            }}
+                          >
+                            {/* Trash-icon */}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              height="36px"
+                              viewBox="0 0 24 24"
+                              width="36px"
+                              fill="#000000"
+                            >
+                              <path d="M0 0h24v24H0V0z" fill="none" />
+                              <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
+                            </svg>
+                          </button>
+                          {/* Edit button */}
+                          <button
+                            className="btn"
+                            title="Click here to edit your post" /* denne vises når du svever over knappen */
+                            onClick={() => {
+                              this.toggleCreatePostWindow();
+                              this.editPost(post);
+                              this.setModalTitle("Edit post");
+                            }}
 
-                          /* TODO: gjør edit-blyanten større onHover: */
-                          /* 
+                            /* TODO: gjør edit-blyanten større onHover: */
+                            /* 
                           onMouseEnter={() => {
                             alert("Event:MouseEnter");
                           }}
                          */
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            height="36px"
-                            viewBox="0 0 24 24"
-                            width="36px"
-                            fill="#000000"
                           >
-                            <path d="M0 0h24v24H0V0z" fill="none" />
-                            <path d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z" />
-                          </svg>
-                        </button>
+                            {/* Edit-icon */}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              height="36px"
+                              viewBox="0 0 24 24"
+                              width="36px"
+                              fill="#000000"
+                            >
+                              <path d="M0 0h24v24H0V0z" fill="none" />
+                              <path d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z" />
+                            </svg>
+                          </button>
+                        </div>
                       ) : null}
                     </div>
                   ) : null}
@@ -224,8 +288,14 @@ export class Posts extends Component {
                 {post.description}
                 <br />
               </CardText>
-
-              {isAuthenticated ? (
+              {/* IF hidden --> already sold. ELSE (IF isAuthenticated --> show contactInfo. ELSE --> log in to show Contactinfo.) */}
+              {post.hidden ? (
+                <div>
+                  <label>
+                    Contact: This post has already been sold/bought.
+                  </label>
+                </div>
+              ) : isAuthenticated ? (
                 <div>
                   <label>
                     {/* Todo: dette kan umulig være rett måte å få mellomrom etter "Contact" :] */}
